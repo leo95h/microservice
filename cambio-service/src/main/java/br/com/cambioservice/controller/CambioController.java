8b0303ad54b0d69fd27f88d8ff0073603a2138d1
leo95h <leonardo.lhpr@gmail.com>
2021-10-10 21:51:01 -0300

    Add modulo Cambio

---
+++ cambio-service/src/main/java/br/com/cambioservice/controller/CambioController.java
@@ -0,0 +1,48 @@
+package br.com.cambioservice.controller;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cambioservice.model.Cambio;
+import br.com.cambioservice.repository.CambioRepository;
+
+@RestController
+@RequestMapping("cambio-service")
+public class CambioController {
+	
+	@Autowired
+	private Environment environment;
+	
+	@Autowired
+	private CambioRepository cambioRepository;
+
+	@GetMapping(value = "/{amount}/{from}/{to}")
+	public Cambio getCambio(@PathVariable("amount") BigDecimal amount,
+			                @PathVariable("from") String from,
+			                @PathVariable("to") String to) {
+		
+		var cambio = cambioRepository.findByFromAndTo(from,to);
+		
+		if (cambio == null) {
+			throw new RuntimeException("Currency Unsupported");
+		}
+		
+		var port = environment.getProperty("local.server.port");
+		cambio.setEnvironment(port);
+		
+		var conversionFactor = cambio.getConversionFactor();
+		var convertedValue = conversionFactor.multiply(amount);
+		
+		cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
+		
+		return cambio;
+	}
+	
+}
---
+++ cambio-service/src/main/java/br/com/cambioservice/model/Cambio.java
@@ -0,0 +1,48 @@
+package br.com.cambioservice.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity(name = "cambio")
+public class Cambio implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "from_currency", nullable = false, length = 3)
+	private String from;
+	
+	@Column(name = "to_currency", nullable = false, length = 3)
+	private String to;
+	
+	@Column(nullable = false)
+	private BigDecimal conversionFactor;
+	
+	@Transient
+	private BigDecimal convertedValue;
+	
+	@Transient
+	private String environment;
+	
+}
